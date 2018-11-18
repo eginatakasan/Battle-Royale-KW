d67e15249634314d32d0488c78d38391a1231552
Egi <eginatakasan@gmail.com>
2018-11-18 20:59:37 +0700

    nambah komen doang, jof, player_at dibikin random ya

--- try.pl
+++ try.pl
@@ -1,26 +1,30 @@
-:- dynamic(player_at/2).
+/* Compiler: GNU Prolog */
+/* :-dynamic diperlukan untuk buat fakta yang bisa diubah */
+/* retract artinya menghapus fakta atau rule dari database */
+/* asserta artinya menambah fakta atau rule dari database ditaruh di awal database*/
+/* assertz artinya menambah fakta atau rule dari database ditaruh di akhir database*/
+:- dynamic(player_at/2). 
 :- dynamic(is_deadzone/2).
+:- dynamic(timer/1).
 
+/* Deklarasi fakta statik */
 enemy_at(2,2).
 ammo_at(2,3).
 weapon_at(2,4).
 medicine_at(3,2).
 armor_at(3,4).
-player_at(3,3).
 
+/* Deklarasi fakta dinamik */
+player_at(3,3).
+timer(1).
+
+/* Deklarasi rules */
 map :- drawmap(15,15).
 
 n :- go(n).
 e :- go(e).
 s :- go(s).
 w :- go(w).
-
-shrink_map(Z) :-
-	A is 15-Z,
-	asserta( (is_deadzone(X,_) :- X =:= Z+1) ),
-	asserta( (is_deadzone(X,_) :- X =:= A) ),
-	assertz( (is_deadzone(_,Y) :- Y =:= Z+1) ),
-	assertz( (is_deadzone(_,Y) :- Y =:= A) ).
 
 look :- 
 	player_at(X,Y),
@@ -37,6 +41,54 @@ look :-
 	show_whats_around(D,B),
 	show_whats_around(D,Y),
 	show_whats_around(D,C),nl.
+
+go(n) :- 
+	retract(player_at(X,Y)), 
+	Z is X-1, 
+	asserta(player_at(Z,Y)),
+	update.
+
+go(e) :- 
+	retract(player_at(X,Y)), 
+	Z is Y+1, 
+	asserta(player_at(X,Z)),
+	update.
+go(s) :- 
+	retract(player_at(X,Y)), 
+	Z is X+1, 
+	asserta(player_at(Z,Y)),
+	update.
+go(w) :- 
+	retract(player_at(X,Y)), 
+	Z is Y-1, 
+	asserta(player_at(X,Z)),
+	update.
+
+update :-
+	timer(X),
+	Z is X mod 7,
+	Z =:= 0,
+	Y is X+1,
+	V is X / 7,!,
+	shrink_map(V),
+	retract(timer(X)),
+	asserta(timer(Y)).
+
+update :-
+	timer(X),
+	Z is X mod 7,
+	Z \= 0,
+	Y is X+1,!,
+	retract(timer(X)),
+	asserta(timer(Y)).
+
+shrink_map(Z) :-
+	A is 15-Z,
+	asserta( (is_deadzone(X,_) :- X =:= Z+1) ),
+	asserta( (is_deadzone(X,_) :- X =:= A) ),
+	assertz( (is_deadzone(_,Y) :- Y =:= Z+1) ),
+	assertz( (is_deadzone(_,Y) :- Y =:= A) ).
+
 
 show_whats_around(X,Y) :- is_deadzone(X,Y), write('X'),!.
 show_whats_around(X,Y) :- enemy_at(X,Y), write('E'),!.
@@ -58,23 +110,4 @@ drawmap(X,Y) :- is_deadzone(X,Y), X >0, Y>0,!, write('X'),nl,!, drawmap(X-1,15),
 drawmap(X,Y) :- player_at(U,V),T is 16-U, X =:= T, S is 16-V, S =:= Y, X>0 , Y>0,!,  write('P'), drawmap(X,Y-1).
 drawmap(X,Y) :- X>1,Y>0,!, write('-'), drawmap(X,Y-1).
 
-go(n) :- 
-	retract(player_at(X,Y)), 
-	Z is X-1, 
-	asserta(player_at(Z,Y)).
-
-go(e) :- 
-	retract(player_at(X,Y)), 
-	Z is Y+1, 
-	asserta(player_at(X,Z)).
-
-go(s) :- 
-	retract(player_at(X,Y)), 
-	Z is X+1, 
-	asserta(player_at(Z,Y)).
-
-go(w) :- 
-	retract(player_at(X,Y)), 
-	Z is Y-1, 
-	asserta(player_at(X,Z)).
 
