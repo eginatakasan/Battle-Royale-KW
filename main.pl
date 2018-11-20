033dac9c7d87c5c5a0c0819248b8666a8278e17a
Egi <eginatakasan@gmail.com>
2018-11-21 03:11:46 +0700

    unequip, take fix, + counter_attacl

--- main.pl
+++ main.pl
@@ -1,3 +1,5 @@
+:- include('winston.pl').
+
 /* Compiler: GNU Prolog */
 /* :-dynamic diperlukan untuk buat fakta yang bisa diubah */
 /* retract artinya menghapus fakta atau rule dari database */
@@ -27,7 +29,7 @@ enemy_at(5,1,5).
 enemy_at(6,1,6).
 
 /*deklarasi item*/
-enemy(1,100,20,ak47).
+enemy(1,100,20,ak47).  /* enemy(NomorEnemy,HealthEnemy,ArmorEnemy,WeaponEnemy) */
 enemy(2,100,5,pistol).
 enemy(3,100,20,ak47).
 enemy(4,100,5,hand).
@@ -35,7 +37,7 @@ enemy(5,100,20,frying_pan).
 enemy(6,100,5,chicken).
 item(weapon,ak47).
 item(weapon,pistol).
-item(weapon,ayam).
+item(weapon,chicken).
 item(weapon,frying_pan).
 item(weapon,shotgun).
 item(weapon,hand).
@@ -51,7 +53,7 @@ item(ammo,shotgun_ammo).
 /*weapon(nama,kekuatan,byk ammo)*/
 weapon(ak47,50,2).
 weapon(pistol,35,1).
-weapon(ayam,25,0).
+weapon(chicken,25,0).
 weapon(frying_pan,30,0).
 weapon(shotgun,60,3).
 weapon(hand,10,0).
@@ -75,16 +77,18 @@ item_at(medicine,medicine,3,2).
 /* Deklarasi fakta dinamik */
 player_at(3,3).
 timer(6).
-equipped_weapon(hand,0).
-equipped_armor(none).
 health(100).
-enemy(1,100,20,ak47). /* enemy(NomorEnemy,HealthEnemy,ArmorEnemy,WeaponEnemy) */
+equipped_weapon(pistol,0).
+equipped_armor(none).
+
 
 /* Deklarasi rules */
 inventory(pistol_ammo,3).
 inventory(pistol_ammo,3).
 inventory(pistol_ammo,3).
 inventory(pistol).
+inventory(medicine).
+
 
 /*fungsi map*/
 map :- drawmap(15,15).
@@ -338,7 +342,7 @@ take(X) :-
 	asserta(inventory(X)),
 	retract(item_at(F,X,Y,Z)),
 	write(X), write(' diambil'), !. 
-take(X) :- 
+take(_) :- 
 	findall(A,inventory(A),B),
 	panjang(B,N),
 	findall(C,inventory(_,C),D),
@@ -349,6 +353,13 @@ take(X) :-
 	write('Tidak ada '), write(X), write(' di lokasi ini'), nl, !, fail.
 
 /*untuk melihat list inventory*/
+lihatinv :- 
+	findall(A,inventory(A),Inv), 
+	findall(B,inventory(B,_),Inve), 
+	konso(Inve,Inv,List), panjang(List,X),
+	X =:= 0,
+	write('Your inventory is empty'),!.
+
 lihatinv :- 
 	findall(A,inventory(A),Inv), 
 	findall(B,inventory(B,_),Inve), 
@@ -380,50 +391,102 @@ drop(X) :-
 
 /*fungsi use*/
 use(X) :- 
-	weapon(X,_,_), 
+	weapon(X,_,_),
+	equipped_weapon(hand,Y), 
 	inventory(X),
 	retract(inventory(X)),
-	equipped_weapon(Y,Z),
-	Y == hand,
-	asserta(equipped_weapon(X,Z)),
+	retract(equipped_weapon(hand,Y)),
+	asserta(equipped_weapon(X,0)),
 	write('Weaponmu sekarang adalah '), write(X), nl, !.
 use(X) :- 
 	weapon(X,_,_), 
 	inventory(X),
-	retract(inventory(X)),
-	equipped_weapon(Y,Z),
+	equipped_weapon(Y,Z), Z =:= 0,
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N > 4,  
 	retract(equipped_weapon(Y,Z)),
-	asserta(equipped_weapon(X,Z)),
-	asserta(inventory(Y)), 
+	retract(inventory(X)),
+	asserta(equipped_weapon(X,0)),
+	asserta(inventory(Y)),
 	write('Weaponmu sekarang adalah '), write(X), nl, !.
 use(X) :- 
 	weapon(X,_,_), 
+	inventory(X),
+	equipped_weapon(Y,Z), Z \= 0, ammo(Ammo,Y,_),
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N > 4,  
+	retract(equipped_weapon(Y,Z)),
+	retract(inventory(X)),
+	asserta(equipped_weapon(X,0)),
+	asserta(inventory(Y)), asserta(inventory(Ammo)), write('Inventory penuh. '),
+	drop(Ammo),!,
+	write('Weaponmu sekarang adalah '), write(X), nl, !.
+use(X) :- 
+	weapon(X,_,_), 
+	inventory(X),
+	equipped_weapon(Y,Z), ammo(Ammo,Y,_), Z \=0,
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N < 4,  
+	retract(inventory(X)),
+	retract(equipped_weapon(Y,Z)),
+	asserta(equipped_weapon(X,0)),
+	asserta(inventory(Y)), asserta(inventory(Ammo)),
+	write('Weaponmu sekarang adalah '), write(X), nl, !.
+use(X) :- 
+	weapon(X,_,_),
 	\+inventory(X), 
 	write('Tidak ada '), write(X), write(' dalam inventory.'), nl, !, fail.
 use(X) :- 
 	armor(X,_),
 	inventory(X),
-	findall(A,equipped_armor(A),B),
-	panjang(B,N),
-	N=0,
+	equipped_weapon(none),
+	retract(equipped_armor(none)), 
 	retract(inventory(X)),
 	asserta(equipped_armor(X)), 
 	write('Armormu sekarang adalah '), write(X), nl, !.
 use(X) :- 
 	armor(X,_), 
-	inventory(X), 
-	retract(inventory(X)), 
+	inventory(X),  
 	equipped_armor(Y),
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N < 5,
 	retract(equipped_armor(Y)), 
+	retract(inventory(X)),
 	asserta(equipped_armor(X)),
 	asserta(inventory(Y)),
+	write('Armormu sekarang adalah '), write(X), nl, !.
+use(X) :- 
+	armor(X,_), 
+	inventory(X),  
+	equipped_armor(Y),
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N > 4,
+	retract(inventory(X)),
+	retract(equipped_armor(Y)), 
+	asserta(equipped_armor(X)),
+	asserta(inventory(Y)), write('Inventory penuh. '),drop(Y),
 	write('Armormu sekarang adalah '), write(X), nl, !.
 use(X) :- 
 	armor(X,_), 
 	\+inventory(X), 
 	write('Tidak ada '), write(X), write(' dalam inventory.'), nl, !, fail.
 use(X) :-
-	medicine(X,B),
+	medicine(X,_),
 	inventory(X),
 	health(A),
 	A=100,
@@ -434,7 +497,7 @@ use(X) :-
 	health(A),
 	A+30>=100,
 	retract(inventory(X)),
-	retract(health(M)),
+	retract(health(A)),
 	asserta(health(100)),
 	write('health berhasil ditambah, health anda sekarang adalah 100'),!.
 use(X) :-
@@ -444,11 +507,11 @@ use(X) :-
 	A+30<100,
 	Z is A + 30,
 	retract(inventory(X)),
-	retract(health(M)),
+	retract(health(A)),
 	asserta(health(Z)),
-	write('health berhasil ditambah, health anda sekarang adalah'), write(Z),!.
+	write('health berhasil ditambah, health anda sekarang adalah '), write(Z),!.
 use(X) :-
-	medicine(X,B),
+	medicine(X,_),
 	\+inventory(X),
 	write('Tidak ada '), write(X), write(' dalam inventory.'), nl, !, fail.
 /*use ammo*/
@@ -468,11 +531,11 @@ use(X) :-
 	retract(equipped_weapon(Weapon,Bykammo)),
 	asserta(equipped_weapon(Weapon,Z)), !.	
 use(X) :-
-	ammo(X,Weapon,N),	
+	ammo(X,_,_),	
 	\+inventory(X),
 	write('Tidak ada'), write(X), write(' dalam inventory'), nl , !, fail.
 use(X) :-
-	ammo(X,Weapon,N),
+	ammo(X,Weapon,_),
 	inventory(X),
 	\+equipped_weapon(Weapon,_),
 	write(X), write(' bukan merupakan ammo dari '), write(Weapon), nl, !, fail.
@@ -481,48 +544,181 @@ use(X) :-
 /*fungsi unequip*/
 unequip(none) :- !.
 unequip(hand) :- !.
-unequip(X) :- equipped_weapon(Y), equipped_armor(Z), X \= Y, X \= Z, write('kamu sedang tidak menggunakan '),write(X),!.
-unequip(X) :- equipped_weapon(X), retract(equipped_weapon(X)), asserta(equipped_weapon(hand)), asserta(inventory(X)), drop(X),!.
-unequip(X) :- equipped_armor(X), retract(equipped_armor(X)), asserta(equipped_armor(none)), asserta(inventory(X)), drop(X),!.
+unequip(X) :- equipped_weapon(Y,_), equipped_armor(Z), X \= Y, X \= Z, write('kamu sedang tidak menggunakan '),write(X),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z =:=0, 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N >= 4, 
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),  
+	asserta(inventory(X)),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z =:=0, 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N > 4,
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),
+	asserta(inventory(X)), write('Inventory penuh. '),
+	drop(X),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z <4, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N < 4, 
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),  
+	asserta(inventory(X)), asserta(inventory(Ammo)),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z >3, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N < 3, 
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),  
+	asserta(inventory(X)), asserta(inventory(Ammo)),asserta(inventory(Ammo)),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z <4, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N =:= 3, 
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),  
+	asserta(inventory(X)), asserta(ammo),write('Inventory penuh. '),drop(Ammo),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z >3, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N =:= 3, 
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),  
+	asserta(inventory(X)), asserta(inventory(ammo)),asserta(inventory(ammo)),write('Inventory penuh. '),drop(Ammo),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z >3, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N =:= 4, 
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),  
+	asserta(inventory(X)), asserta(ammo),asserta(ammo),write('Inventory penuh. '),drop(Ammo),drop(Ammo),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z <4, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N >= 5,
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),
+	asserta(inventory(X)), write('Inventory penuh. '),
+	drop(X),drop(Ammo),!.
+unequip(X) :- 
+	equipped_weapon(X,Z), Z > 0, Z >3, ammo(Ammo,X,_), 
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N >= 5,
+	retract(equipped_weapon(X,Z)), 
+	asserta(equipped_weapon(hand,0)),
+	asserta(inventory(X)), asserta(inventory(Ammo)), asserta(inventory(Ammo)), write('Inventory penuh. '),
+	drop(X),drop(Ammo),drop(Ammo),!.
+unequip(X) :- 
+	equipped_armor(X), 
+	retract(equipped_armor(X)), 
+	asserta(equipped_armor(none)),
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N =< 5, 
+	asserta(inventory(X)),!.
+unequip(X):-
+	equipped_armor(X), 
+	retract(equipped_armor(X)), 
+	asserta(equipped_armor(none)),
+	findall(A,inventory(A),B),
+	panjang(B,N),
+	findall(C,inventory(_,C),D),
+	panjang(D,M),
+	M+N > 4,  asserta(inventory(X)), write('Inventory penuh. '),
+	drop(X),!.
 
-/*fungsi attack (belum lengkap)*/
+/* fungsi attack */
 attack :- 
 	player_at(X,Y),
 	\+enemy_at(_,X,Y),
-	write('Tidak ada musuh dalam jarak yang dekat1!'), !, fail.
+	write('Tidak ada musuh dalam jarak yang dekat!'), !, fail.
 attack :- 
 	player_at(X,Y),
-	enemy_at(_,X,Y),
+	enemy_at(NomorEnemy,X,Y),
+	enemy(NomorEnemy,_,_,WeaponEnemy), 
 	equipped_weapon(Weapon,JlhAmmo),
 	weapon(Weapon,_,AmmoNeeded),
 	JlhAmmo < AmmoNeeded,
-	write('Ammo tidak cukup!'), !.
+	write('Ammo tidak cukup! '),nl, 
+	write('Musuh menyerang kembali!'),nl,
+	health(Health), equipped_armor(NamaArmor), armor(NamaArmor,Armor), weapon(WeaponEnemy,DmgEnemy,_),
+	calculate_health(Health,Armor,DmgEnemy),
+	health(NewHealth),
+	write('Health kamu tinggal '), write(NewHealth), nl, !.
+
 attack :- 
 	player_at(X,Y), 
-	health(Health),
 	enemy_at(NomorEnemy,X,Y),
-	enemy(NomorEnemy,HealthEnemy,ArmorEnemy,WeaponEnemy),
-	equipped_armor(NamaArmor),
-	armor(NamaArmor,Armor),
+	enemy(NomorEnemy,HealthEnemy,ArmorEnemy,_),
 	equipped_weapon(Weapon,JlhAmmo),
 	weapon(Weapon,Dmg,AmmoNeeded),
-	weapon(WeaponEnemy,DmgEnemy,_),
 	JlhAmmo >= AmmoNeeded,
 	write('Kamu menyerang musuh dengan '), write(Weapon), write('. '), nl,
 	calculate_damage(NomorEnemy,HealthEnemy,ArmorEnemy,Dmg),
 	write('Health musuh tersisa '),
 	enemy(NomorEnemy,B,_,_),
 	write(B), nl,
+	counter_attack(NomorEnemy), !.
+
+/* counter_attack (BELUM LENGKAP): musuh melakukan attack pada player jika musuh diserang, atau musuh mati setelah diserang */
+counter_attack(NomorEnemy) :-
+	enemy(NomorEnemy,HealthEnemy,_,WeaponEnemy),
+	HealthEnemy > 0,
+	health(Health), equipped_armor(NamaArmor),
+	armor(NamaArmor,Armor), weapon(WeaponEnemy,DmgEnemy,_),
 	write('Musuh menyerang kembali!'),nl,
 	calculate_health(Health,Armor,DmgEnemy),
 	health(NewHealth),
-	write('Health kamu tinggal '), write(NewHealth), nl, !.
+	write('Health kamu tinggal '), write(NewHealth), nl,!.
 
-/*attackmusuh*/
+counter_attack(NomorEnemy) :-
+	enemy(NomorEnemy,HealthEnemy,ArmorEnemy,WeaponEnemy),
+	HealthEnemy =:= 0,
+	write('Musuh telah mati! '),nl,
+	enemy_at(NomorEnemy,X,Y), write('1'),
+	retract(enemy_at(NomorEnemy,X,Y)),
+	retract(enemy(NomorEnemy,HealthEnemy,ArmorEnemy,WeaponEnemy)),
+	asserta(item_at(armor,ArmorEnemy,X,Y)), write('armor'),
+	asserta(item_at(weapon,WeaponEnemy,X,Y)), write('weapon'),
+	ammo(NamaAmmo,WeaponEnemy,_), write('a'),
+	asserta(item_at(ammo,NamaAmmo,X,Y)), write('b'),
+	write('Musuh telah menjatuhkan inventorynya:'),nl,
+	konso(WeaponEnemy,ArmorEnemy,List1), konso(List1,NamaAmmo,List2),
+	write(List2),nl,!.
 
 /*calculate_health*/
-calculate_health(Health,Armor,Dmg) :-
-	X is Dmg - Armor, X =< 0,!.
 
 calculate_health(Health,Armor,Dmg) :-
 	X is Dmg - Armor, X > 0,
@@ -538,6 +734,9 @@ calculate_health(Health,Armor,Dmg) :-
 	Y is Health - X,
 	retract(health(Health)),
 	asserta(health(Y)),!.
+
+calculate_health(_,Armor,Dmg) :-
+	X is Dmg - Armor, X =< 0,!.
 
 
 /* fungsi calculate_damage: menghitung damage yang dilakukan kepada musuh */
--- winston.pl
+++ winston.pl
@@ -2,10 +2,4 @@ save(X) :- open('save_file.pl',write,Stream),health(X), armor(_,Y), equipped_wea
 
 load(X) :- consult('save_file.pl'),['save_file'].
 
-status :- health(X), armor(_,Y), equipped_weapon(Z), ammo(_,_,S),inventory(T), Z \= 'none',T \= 'none', write('Health: '),write(X),nl, write('Armor: '), write(Y),nl,write('Weapon: '),write(Z),nl,write('Ammo: '),write(S),nl,write('Inventory:'),nl,write(T),nl.
-
-status :- health(X), armor(_,Y), equipped_weapon(Z), ammo(_,_,S),inventory(T), Z \= 'none',T == 'none',write('Health: '),write(X),nl, write('Armor: '), write(Y),nl,write('Weapon: '),write(Z),nl,write('Ammo: '),write(S),nl,write('Your inventory is empty'),nl.
-
-status :- health(X), armor(_,Y), equipped_weapon(Z), ammo(_,_,S),inventory(T), Z == 'none',T \= 'none', write('Health: '),write(X),nl, write('Armor: '), write(Y),nl,write('Weapon: '),write(Z),nl,write('Inventory:'),nl,write(T),nl.
-
-status :- health(X), armor(_,Y), equipped_weapon(Z), ammo(_,_,S),inventory(T), Z == 'none',T == 'none', write('Health: '),write(X),nl, write('Armor: '), write(Y),nl,write('Weapon: '),write(Z),nl,write('Your inventory is empty'),nl.
+status :- health(X), equipped_armor(NamaArmor),armor(NamaArmor,Y), equipped_weapon(Z,S), write('Health: '),write(X),nl, write('Armor: '), write(Y),nl,write('Weapon: '),write(Z),nl,write('Ammo: '),write(S),nl,write('Inventory:'),nl,lihatinv,nl.
